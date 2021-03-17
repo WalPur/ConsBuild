3ea01d5364d156a579815942f31a2449b8fd25e5
Misha <mishakamba1@gmail.com>
2021-03-17 16:37:29 +0900

    models added and rest deploy

--- web/models.py
+++ web/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.TextField(max_length=255, verbose_name="Категория товаров")
+
+class Exporter(models.Model):
+    name = models.TextField(max_length=255, verbose_name="Название продавца")
+    address = models.TextField(max_length=255, verbose_name="Адрес продавца")
+    tel = models.TextField(max_length=255, verbose_name="Телефон продавца")
+
+class Lot(models.Model):
+    name = models.TextField(max_length=255, verbose_name="Название товара")
+    material = models.TextField(max_length=255, verbose_name="Материал товара")
+    purpose = models.TextField(max_length=255, verbose_name="Назначение товара")
+    shop = models.ForeignKey("Exporter", verbose_name="Поставщик товара", on_delete=models.CASCADE)
+    category = models.ForeignKey("Category", verbose_name="Категория товара", on_delete=models.CASCADE)
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    surname = models.CharField(max_length=500, verbose_name="Фамилия")
+    name = models.CharField(max_length=500, verbose_name="Имя")
+    patronymic = models.CharField(max_length=500, verbose_name="Отчество")
+
+    person_type = models.TextField(max_length=255, verbose_name="Лицо")
+
+    cart = models.ManyToManyField("Lot", verbose_name="Корзина", related_name="cart")
+    favorite = models.ManyToManyField("Lot", verbose_name="Избранное", related_name="favorite")
